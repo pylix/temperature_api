5186394c8684063b862722c36da0e898bbd4921f
Alix Etienne <pylixx@gmail.com>
2022-05-14 01:42:35 -0400

    TAPI-4 add list of options and absolute 0 checks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from enum import Enum
 
 from fastapi import FastAPI, HTTPException
 #from typing import Literal
@@ -26,33 +27,56 @@ tags_metadata = [
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Temperature Converter API",
-            "endpoints": ["/convert"],
-            "documentation": "/documentation",
-            "open-api-doc": "/openapi.json"
-            }
+    return {
+        "message": "Welcome to the Temperature Converter API",
+        "endpoints": ["/convert"],
+        "documentation": "/documentation",
+        "open-api-doc": "/openapi.json"
+    }
 
 
 @app.get("/convert")
 async def convert():
-    return {"message": "Use to convert temperatures",
-            "documentation": "/documentation",
-            "open-api-doc": "/openapi.json"
-            }
+    return {
+        "message": "This endpoint is Used to convert temperatures",
+        "documentation": "/documentation",
+        "open-api-doc": "/openapi.json"
+    }
+
+
+class TempEnum(str, Enum):
+    celsius = 'celsius'
+    fahrenheit = 'fahrenheit'
+    C = 'C'
+    F = 'F'
+    Celsius = 'Celsius'
+    Fahrenheit = 'Fahrenheit'
 
 
 @app.get("/convert/{temperature}/{unit}/{convertTo}")
 async def convert_temperature(
-        temperature: Decimal, unit: str, convertTo: str):
-    if (unit.lower() in ('celsius', 'c')
-            and convertTo.lower() in ('fahrenheit', 'f')):
-        converted_temp = temperature * Decimal('1.8') + 32
-    elif (unit.lower() in ('fahrenheit', 'f')
-            and convertTo.lower() in ('celsius', 'c')):
-        converted_temp = (temperature - Decimal(32)) * (Decimal(5)/Decimal(9))
-    else:
+        temperature: Decimal,
+        unit: TempEnum, convertTo: TempEnum):
+    if unit.lower() in ('fahrenheit', 'f') and temperature < Decimal(-459.67):
         raise HTTPException(
             status_code=400,
-            detail=f"Only Celsius and Fahrenheit are supported"
+            detail=f"Values lower than absolute zero are not allowed"
+        )
+    elif unit.lower() in ('celsius', 'c') and temperature < Decimal(-273.15):
+        raise HTTPException(
+            status_code=400,
+            detail=f"Values lower than absolute zero are not allowed"
+        )
+    elif (unit.lower() in ('celsius', 'c')
+            and convertTo.lower() in ('fahrenheit', 'f')):
+        converted_temp = Decimal(temperature * Decimal('1.8') + 32)
+    elif (unit.lower() in ('fahrenheit', 'f')
+            and convertTo.lower() in ('celsius', 'c')):
+        converted_temp = Decimal(temperature - Decimal(32)) * (Decimal(5)/Decimal(9))
+    else:
+        # this code block should only be reached when the same unit appears twice
+        raise HTTPException(
+            status_code=400,
+            detail=f"Nothing to convert because unit and convertTo are the unit"
         )
     return {"value": converted_temp}
